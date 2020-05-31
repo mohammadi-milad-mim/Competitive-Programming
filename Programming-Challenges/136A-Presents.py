ae877de717ec97bf0c1bddccb11306de7f9678d0
MilladMuhammadi <43762761+MilladMuhammadi@users.noreply.github.com>
2020-05-31 11:29:41 +0430

    Delete 136A-Presents.py

--- Programming-Challenges/136A-Presents.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-mainList = list(map(int, input().split()))
-secList = mainList.copy()
-mainList.sort()
-plist=[]
-for i in range(n):
-    print(secList.index(mainList[i])+1, end =" ")
